c21c3b197709f9ab2e6776db651de11aecc863fc
Mikael Souza <mikael.souza.cc@gmail.com>
2024-02-22 23:52:15 -0400

    work in progress

--- src/handlers.rs
+++ src/handlers.rs
@@ -6,15 +6,15 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Transaction {
-    valor: u128,
+    valor: i64,
     tipo: String, // TODO: Use an enum in the future
     descricao: String,
 }
 
 #[derive(Debug, Serialize)]
 pub struct TransactionResponse {
-    limite: u128,
-    saldo: u128,
+    limite: i64,
+    saldo: i64,
 }
 
 #[debug_handler]
@@ -33,4 +33,42 @@ pub async fn post_transaction(
     log::info!("Transaction values: {}", transaction);
     log::info!("Response: {}", response);
     (StatusCode::OK, response)
+}
+
+/*
+    TODO:
+    How can I reuse the already existing transacion?
+    Maybe I should only create a Transacion and insert the
+    `realizado_em` field there.
+*/
+
+pub struct RecordedTransaction {
+    valor: i64,
+    tipo: String,
+    descricao: String,
+    realizado_em: String,
+}
+
+pub struct Statement {
+    total: i64,
+    data_extrato: String, // # TODO: Change this to a date format
+    limite: i64,
+    ultimas_transacoes: Vec<RecordedTransaction>,
+}
+
+impl Statement {
+    fn init() -> Self {
+        Self {
+            total: 0,
+            data_extrato: String::from(""),
+            limite: 0,
+            ultimas_transacoes: Vec::new(),
+        }
+    }
+}
+
+pub async fn get_statement(Path(user_id): Path<u32>) -> impl IntoResponse {
+    todo!();
+    let s = Statement::init();
+    (StatusCode::OK, format!(""))
 }
--- src/setup.rs
+++ src/setup.rs
@@ -1,10 +1,10 @@
+use crate::handlers;
+use axum::routing::{get, post};
+use axum::Router;
 use envconfig::Envconfig;
 use std::net::{Ipv4Addr, SocketAddrV4};
-
-use crate::handlers;
-use axum::routing::post;
-use axum::Router;
 use tokio::net::TcpListener;
+
 #[derive(Envconfig, Debug)]
 pub struct Config {
     #[envconfig(from = "HTTP_PORT", default = "8080")]
@@ -18,10 +18,12 @@ pub fn init_environment() -> Config {
 }
 
 pub fn init_routes() -> Router {
-    Router::new().route(
-        "/clientes/:user_id/transacoes",
-        post(handlers::post_transaction),
-    )
+    Router::new()
+        .route(
+            "/clientes/:user_id/transacoes",
+            post(handlers::post_transaction),
+        )
+        .route("/clientes/:user_id/extrato", get(handlers::get_statement))
 }
 
 pub async fn init_address(port: u16) -> TcpListener {
