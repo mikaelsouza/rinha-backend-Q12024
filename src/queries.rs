0fc19bfb945dc2f9b642f741e697eaaf84e0959d
Mikael Souza <mikael.souza.cc@gmail.com>
2024-02-24 00:12:30 -0400

    update queries

--- src/queries.rs
+++ src/queries.rs
@@ -1,4 +1,4 @@
-pub const CURRENT_BALANCE: &str = r#"
+pub const GET_CURRENT_BALANCE: &str = r#"
     SELECT
         "limit"    AS "limit",
         NOW()      AS "timestamp",
@@ -6,7 +6,7 @@ pub const CURRENT_BALANCE: &str = r#"
     FROM accounts
     WHERE id = $1
 "#;
-pub const PREVIOUS_TRANSACTIONS: &str = r#"
+pub const GET_PREVIOUS_TRANSACTIONS: &str = r#"
     SELECT
         value               AS "value",
         transaction_type    AS "transaction_type",
@@ -15,4 +15,9 @@ pub const PREVIOUS_TRANSACTIONS: &str = r#"
     FROM transactions
     WHERE account_id = $1
     ORDER BY timestamp DESC
+"#;
+pub const INSERT_TRANSACTION: &str = r#"
+    INSERT INTO "transactions"
+    ("account_id", "value", "transaction_type", "description", "timestamp")
+    VALUES ($1, $2, $3, $4, NOW())
 "#;
