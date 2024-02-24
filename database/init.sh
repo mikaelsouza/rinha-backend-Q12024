bc9aa2cf2cc433b17e017c05ae5702c4fe649454
Mikael Souza <mikael.souza.cc@gmail.com>
2024-02-23 21:21:57 -0400

    complate database with transcations and accounts tables

--- database/init.sh
+++ database/init.sh
@@ -4,16 +4,27 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE rinha;
     \c rinha
-    CREATE TABLE "transactions" (
+    CREATE TABLE "accounts" (
         "id" serial NOT NULL,
-        PRIMARY KEY ("id"),
         "limit" bigint NOT NULL,
-        "balance" bigint NOT NULL
+        "balance" bigint NOT NULL,
+        PRIMARY KEY ("id")
     );
-    INSERT INTO transactions ("limit", "balance") 
+    INSERT INTO accounts ("limit", "balance")
     VALUES (100000, 0),
        (80000, 0),
        (1000000, 0),
        (10000000, 0),
        (500000, 0);
+    CREATE TYPE transaction_type as ENUM ('c', 'd');
+    CREATE TABLE "transactions" (
+        "id" serial NOT NULL,
+        "account_id" serial NOT NULL,
+        "value" bigint NOT NULL,
+        "type" transaction_type NOT NULL,
+        "description" text NOT NULL,
+        "timestamp" timestamp NOT NULL DEFAULT NOW(),
+        PRIMARY KEY ("id"),
+        FOREIGN KEY("account_id") REFERENCES accounts(id)
+    )
 EOSQL
