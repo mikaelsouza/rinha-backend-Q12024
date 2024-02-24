f4ad1dbc16405cf6c4a36779f2ea1de961248808
Mikael Souza <mikael.souza.cc@gmail.com>
2024-02-23 23:37:29 -0400

    WIP handlers

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,88 +1,35 @@
-use crate::queries;
+use crate::{queries, types};
 use axum::debug_handler;
 use axum::extract::{Json, Path, State};
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
-use serde::{Deserialize, Serialize};
-use sqlx::prelude::FromRow;
-use sqlx::{Pool, Postgres, Type};
-use time::OffsetDateTime;
-
-#[derive(Type, Debug, Serialize, Deserialize)]
-#[sqlx(type_name = "transaction_type", rename_all = "lowercase")]
-enum TransactionType {
-    C,
-    D,
-}
-
-#[derive(Debug, Deserialize, Serialize, FromRow)]
-pub struct Transaction {
-    value: i64,
-    transaction_type: TransactionType,
-    description: String,
-    timestamp: OffsetDateTime,
-}
-#[derive(Debug, Deserialize, Serialize, FromRow)]
-pub struct Statement {
-    balance: i64,
-    timestamp: OffsetDateTime, // # TODO: Change this to a date format
-    limit: i64,
-}
-
-pub struct StatementResponse {
-    statement: Statement,
-    previous_transactions: Vec<Transaction>,
-}
-
-#[derive(Debug, Serialize)]
-pub struct TransactionResponse {
-    limite: i64,
-    saldo: i64,
-}
+use sqlx::{Pool, Postgres};
 
 #[debug_handler]
 pub async fn post_transaction(
     State(state): State<Pool<Postgres>>,
     Path(user_id): Path<u32>,
-    Json(transaction): Json<Transaction>,
+    Json(transaction): Json<types::Transaction>,
 ) -> impl IntoResponse {
-    let transaction = serde_json::to_string(&transaction).unwrap();
-    let response = serde_json::to_string(&TransactionResponse {
-        limite: 10000,
-        saldo: 100,
-    })
-    .unwrap();
-
-    log::info!("User ID: {}", user_id);
-    log::info!("Transaction values: {}", transaction);
-    log::info!("Response: {}", response);
+    log::info!("{:?}", transaction);
+    let response = String::from("Hey!");
     (StatusCode::OK, response)
 }
 
-/*
-    TODO:
-    How can I reuse the already existing transacion?
-    Maybe I should only create a Transacion and insert the
-    `realizado_em` field there.
-*/
-
 #[debug_handler]
-pub async fn get_statement(
-    State(state): State<Pool<Postgres>>,
-    Path(user_id): Path<i32>,
-) -> String {
-    let current_statement: Statement = sqlx::query_as(queries::CURRENT_STATEMENT)
+pub async fn get_balance(State(state): State<Pool<Postgres>>, Path(user_id): Path<i32>) -> String {
+    // Logic for solving the problem
+    let current_balance = sqlx::query_as(queries::CURRENT_BALANCE)
         .bind(user_id)
-        .fetch_one(&state)
-        .await
-        .unwrap();
-
-    let previous_transactions: Vec<Transaction> = sqlx::query_as(queries::PREVIOUS_TRANSACTIONS)
+        .fetch_one(&state);
+    let previous_transactions = sqlx::query_as(queries::PREVIOUS_TRANSACTIONS)
         .bind(user_id)
-        .fetch_all(&state)
-        .await
-        .unwrap();
+        .fetch_all(&state);
+    let current_balance: types::Balance = current_balance.await.unwrap();
+    let previous_transactions: Vec<types::Transaction> = previous_transactions.await.unwrap();
 
-    let response = format!("{:?}, {:?}", current_statement, previous_transactions);
+    // Generating final response
+
+    let response = format!("{:?}, {:?}", current_balance, previous_transactions);
     response
 }
