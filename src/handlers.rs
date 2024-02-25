c79b3ce16f5ee7e71b8e827c0b8a5164dc202751
Mikael Souza <mikael.souza.cc@gmail.com>
2024-02-24 22:05:40 -0400

    WIP working on queries and checking how it's better to implemet them

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,4 +1,3 @@
-use crate::types::BalanceResponse;
 use crate::{queries, types};
 use axum::debug_handler;
 use axum::extract::{Json, Path, State};
@@ -10,29 +9,65 @@ pub async fn post_transaction(
     Path(user_id): Path<i64>,
     Json(transaction): Json<types::Transaction>,
 ) -> String {
-    sqlx::query(queries::INSERT_TRANSACTION)
+    let current_balance: types::Balance = sqlx::query_as(queries::GET_CURRENT_BALANCE)
         .bind(user_id)
-        .bind(&transaction.value)
-        .bind(&transaction.transaction_type)
-        .bind(&transaction.description)
-        .execute(&state)
+        .fetch_one(&state)
         .await
         .unwrap();
-    // TODO: Finish the logic for this handler
-    serde_json::to_string(&transaction).unwrap()
+
+    let new_balance = current_balance.balance - transaction.value;
+    let limit = -current_balance.limit;
+    if new_balance < limit {
+        return String::from("Sheesh");
+    }
+    let mut db_transaction = state.begin().await.unwrap();
+    sqlx::query(queries::INSERT_TRANSACTION)
+        .bind(user_id)
+        .bind(transaction.value)
+        .bind(transaction.transaction_type)
+        .bind(transaction.description)
+        .execute(&mut *db_transaction)
+        //.execute(&state)
+        .await
+        .unwrap();
+    sqlx::query(queries::SET_NEW_BALANCE)
+        .bind(new_balance)
+        .bind(user_id)
+        .execute(&mut *db_transaction)
+        //.execute(&state)
+        .await
+        .unwrap();
+    db_transaction.commit().await.unwrap();
+    let current_balance: types::Balance = sqlx::query_as(queries::GET_CURRENT_BALANCE)
+        .bind(user_id)
+        .fetch_one(&state)
+        //.fetch_one(&mut *db_transaction)
+        .await
+        .unwrap();
+    //db_transaction.commit().await.unwrap();
+
+    assert_eq!(new_balance, current_balance.balance);
+
+    String::from("Tudo show de bola!")
 }
 
 #[debug_handler]
-pub async fn get_balance(State(state): State<Pool<Postgres>>, Path(user_id): Path<i32>) -> String {
+pub async fn get_balance(State(state): State<Pool<Postgres>>, Path(user_id): Path<i64>) -> String {
     let current_balance = sqlx::query_as(queries::GET_CURRENT_BALANCE)
         .bind(user_id)
-        .fetch_one(&state);
+        .fetch_one(&state)
+        .await
+        .unwrap();
     let previous_transactions = sqlx::query_as(queries::GET_PREVIOUS_TRANSACTIONS)
         .bind(user_id)
-        .fetch_all(&state);
-    let current_balance: types::Balance = current_balance.await.unwrap();
-    let previous_transactions: Vec<types::Transaction> = previous_transactions.await.unwrap();
-    let balance_response = BalanceResponse {
+        .fetch_all(&state)
+        .await
+        .unwrap();
+
+    // TODO: Verify which one is more idiomatic
+    // let current_balance = queries::get_current_balance(user_id, state).await;
+    // let previous_transactions = queries::get_previous_transactions(user_id, state)
+    let balance_response = types::BalanceResponse {
         balance: current_balance,
         previous_transactions,
     };
--- src/queries.rs
+++ src/queries.rs
@@ -21,3 +21,74 @@ pub const INSERT_TRANSACTION: &str = r#"
     ("account_id", "value", "transaction_type", "description", "timestamp")
     VALUES ($1, $2, $3, $4, NOW())
 "#;
+
+pub const SET_NEW_BALANCE: &str = r#"
+    UPDATE accounts
+    SET balance = $1
+    WHERE id = $2
+"#;
+
+use sqlx::{Pool, Postgres};
+
+use crate::types;
+
+pub async fn get_current_balance(user_id: i64, pool: Pool<Postgres>) -> types::Balance {
+    sqlx::query_as!(
+        types::Balance,
+        r#"
+        SELECT
+            "limit"    AS "limit",
+            NOW()      AS "timestamp!",
+            balance    AS "balance"
+        FROM accounts
+        WHERE id = $1
+        "#,
+        user_id
+    )
+    .fetch_one(&pool)
+    .await
+    .unwrap()
+}
+
+pub async fn get_previous_transactions(
+    user_id: i64,
+    state: Pool<Postgres>,
+) -> Vec<types::Transaction> {
+    sqlx::query_as!(
+        types::Transaction,
+        r#"
+        SELECT
+            value               AS "value",
+            transaction_type    AS "transaction_type",
+            description         AS "description",
+            timestamp           AS "timestamp"
+        FROM transactions
+        WHERE account_id = $1
+        ORDER BY timestamp DESC
+        "#,
+        user_id
+    )
+    .fetch_all(&state)
+    .await
+    .unwrap()
+}
+
+pub async fn insert_transaction(
+    user_id: i32,
+    transaction: types::Transaction,
+    state: Pool<Postgres>,
+) -> types::Balance {
+    sqlx::query!(
+        r#"
+        INSERT INTO "transactions"
+        ("account_id", "value", "transaction_type", "description", "timestamp")
+        VALUES ($1, $2, $3, $4, NOW())
+        "#,
+        user_id,
+        transaction.value,
+        transaction.transaction_type,
+        transaction.description
+    )
+    .await
+    .unwrap()
+}
