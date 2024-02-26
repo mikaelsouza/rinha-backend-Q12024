455ac2e642069fa285dac285811abf4ea580d5db
Mikael Souza <mikael.souza.cc@gmail.com>
2024-02-25 20:06:04 -0400

    WIP general logic working

--- src/handlers.rs
+++ src/handlers.rs
@@ -5,48 +5,31 @@ use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use sqlx::{Pool, Postgres};
 
+// lmao this is terrible, please kill me
 #[debug_handler]
 pub async fn post_transaction(
     State(pool): State<Pool<Postgres>>,
     Path(user_id): Path<i64>,
     Json(transaction): Json<types::Transaction>,
 ) -> impl IntoResponse {
-    todo!();
-    // // Check if the user exists
-    // let current_balance = match queries::get_current_balance(user_id, &pool).await {
-    //     Ok(balance) => balance,
-    //     Err(sqlx::Error::RowNotFound) => return (StatusCode::NOT_FOUND, String::from("{}")),
-    //     Err(_) => panic!(),
-    // };
+    if queries::check_user_exists(user_id, &pool).await == false {
+        return (StatusCode::NOT_FOUND, String::from("{}"));
+    }
 
-    // // Calculate new balance
-    // let new_balance = current_balance.balance - transaction.value;
-    // let limit = -current_balance.limit;
-    // if transaction.transaction_type == types::TransactionType::D && new_balance < limit {
-    //     return (StatusCode::UNPROCESSABLE_ENTITY, String::from("{}"));
-    // }
-
-    // // Update balance
-    // let mut db_transaction = pool.begin().await.unwrap();
-    // queries::insert_transaction(user_id, transaction, &mut *db_transaction)
-    //     .await
-    //     .unwrap();
-    // queries::set_new_balance(user_id, new_balance, &mut *db_transaction)
-    //     .await
-    //     .unwrap();
-    // db_transaction.commit().await.unwrap();
-
-    // // Check if everything is ok. Is this really necessary?
-    // let current_balance = queries::get_current_balance(user_id, &pool).await.unwrap();
-    // assert_eq!(new_balance, current_balance.balance);
-
-    // // Build response
-    // let response = types::TransactionResponse {
-    //     balance: current_balance.balance,
-    //     limit: current_balance.limit,
-    // };
-    // let json_string = serde_json::to_string(&response).unwrap();
-    // (StatusCode::OK, json_string)
+    let add_transaction = queries::add_transaction(user_id, transaction, &pool).await;
+    let current_balance = match add_transaction {
+        Ok(balance) => balance,
+        Err(sqlx::Error::RowNotFound) => {
+            return (StatusCode::UNPROCESSABLE_ENTITY, String::from("{}"))
+        }
+        Err(_) => panic!(),
+    };
+    let response = types::TransactionResponse {
+        balance: current_balance.balance,
+        limit: current_balance.limit,
+    };
+    let json_string = serde_json::to_string(&response).unwrap();
+    (StatusCode::OK, json_string)
 }
 
 #[debug_handler]
@@ -54,21 +37,20 @@ pub async fn get_balance(
     State(pool): State<Pool<Postgres>>,
     Path(user_id): Path<i64>,
 ) -> impl IntoResponse {
-    todo!();
-    // let current_balance = match queries::get_current_balance(user_id, &pool).await {
-    //     Ok(balance) => balance,
-    //     Err(sqlx::Error::RowNotFound) => return (StatusCode::NOT_FOUND, String::from("{}")),
-    //     Err(_) => panic!(),
-    // };
-    // let previous_transactions = queries::get_previous_transactions(user_id, 10, &pool)
-    //     .await
-    //     .unwrap();
-    // let balance_response = types::BalanceResponse {
-    //     balance: current_balance,
-    //     previous_transactions,
-    // };
-    // (
-    //     StatusCode::OK,
-    //     serde_json::to_string(&balance_response).unwrap(),
-    // )
+    let current_balance = match queries::get_current_balance(user_id, &pool).await {
+        Ok(balance) => balance,
+        Err(sqlx::Error::RowNotFound) => return (StatusCode::NOT_FOUND, String::from("{}")),
+        Err(_) => panic!(),
+    };
+    let previous_transactions = queries::get_previous_transactions(user_id, 10, &pool)
+        .await
+        .unwrap();
+    let balance_response = types::BalanceResponse {
+        balance: current_balance,
+        previous_transactions,
+    };
+    (
+        StatusCode::OK,
+        serde_json::to_string(&balance_response).unwrap(),
+    )
 }
--- src/queries.rs
+++ src/queries.rs
@@ -1,9 +1,7 @@
 use crate::types::{self, Balance};
 
-use sqlx::{
-    postgres::{PgExecutor, PgQueryResult},
-    Error,
-};
+use sqlx::postgres::{PgExecutor, PgQueryResult};
+use sqlx::Error;
 
 pub async fn get_current_balance(
     user_id: i64,
@@ -59,7 +57,7 @@ pub async fn insert_transaction(
         r#"
         INSERT INTO "transactions"
             ("account_id", "value", "transaction_type", "description", "timestamp")
-        VALUES 
+        VALUES
             ($1, $2, $3, $4, NOW())
         "#,
         user_id,
@@ -86,5 +84,66 @@ pub async fn set_new_balance(
         user_id
     )
     .execute(executor)
+    .await
+}
+
+pub async fn check_user_exists(user_id: i64, executor: impl PgExecutor<'_>) -> bool {
+    let query_result = sqlx::query_as!(
+        types::Balance,
+        r#"SELECT balance, NOW() as "timestamp!", "limit" FROM accounts WHERE id = $1"#,
+        user_id,
+    )
+    .fetch_one(executor)
+    .await;
+    match query_result {
+        Ok(_) => return true,
+        Err(_) => return false,
+    }
+}
+
+pub async fn add_transaction(
+    user_id: i64,
+    transaction: types::Transaction,
+    executor: impl PgExecutor<'_>,
+) -> Result<Balance, Error> {
+    sqlx::query_as!(types::Balance,
+        r#"
+        WITH account_info AS (
+            SELECT balance, "limit"
+            FROM accounts
+            WHERE id = $1
+        ),
+        new_transaction AS (
+            SELECT
+                $1::bigint AS account_id,
+                $2::bigint AS value,
+                $3::transaction_type AS transaction_type,
+                $4::text AS description,
+                NOW() AS timestamp
+            FROM account_info
+            WHERE ($3 = 'D' AND account_info.balance - $2 >= -account_info.limit)
+               OR ($3 = 'C') -- No need to check limit for credit transactions
+        ),
+        inserted_transaction AS (
+            INSERT INTO "transactions" ("account_id", "value", "transaction_type", "description", "timestamp")
+            SELECT * FROM new_transaction
+            RETURNING "account_id", "value"
+        ),
+        update_balance AS (
+            UPDATE accounts
+            SET balance = CASE
+                WHEN $3 = 'D' THEN balance - COALESCE((SELECT "value" FROM inserted_transaction), 0)
+                ELSE balance + (SELECT "value" FROM inserted_transaction)
+                END
+            WHERE EXISTS (SELECT 1 FROM inserted_transaction) AND id = $1
+            RETURNING balance, "limit"
+        )
+        SELECT *, NOW() AS "timestamp!" FROM update_balance;"#,
+        user_id,
+        transaction.value,
+        transaction.transaction_type as types::TransactionType,
+        transaction.description
+    )
+    .fetch_one(executor)
     .await
 }
